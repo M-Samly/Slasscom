440f174df3ae3a99902c860708592ff670d6c61e
M-Samly <m.shamlymhmd@gmail.com>
2022-12-13 22:51:07 +0530

    first commit

---
+++ Challenge2/Cha2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Cha2{
+
+    public static void main(String[] args){
+        /*Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Brakets here : ");
+        String brac = sc.nextLine();
+        //System.out.println(brac);
+        for(int i = 0;i<brac.length();i++){
+            ss
+        }*/
+        
+    }
+
+}
---
+++ Challenge2/Characters.java
@@ -0,0 +1,15 @@
+public class Characters{
+    private String chara;
+
+    public Characters(String chara){
+        this.chara = chara;
+    }
+
+    public void setChara(String chara){
+        this.chara = chara;
+    }
+
+    public String getChara(){
+        return chara;
+    }
+}
