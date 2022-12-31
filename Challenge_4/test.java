e6239f21d1208db0a1c49934a058bc090440bc5d
M-Samly <m.shamlymhmd@gmail.com>
2022-12-31 20:48:01 +0530

    Updated

--- Challenge_4/test.java
+++ Challenge_4/test.java
@@ -1,10 +1,8 @@
-import java.lang.Math;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Main {
-
+public class test {
     public static void main(String[] args) {
 
         String[] strArray = new String[256];
@@ -13,34 +11,28 @@ public class Main {
         String[] subArray_M = new String[256];
 
         ReadData("test.txt", strArray, ",");
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println(strArray[i]);
+        }
+
         ReadDataSub_E("test.txt", subArray_E, " ");
 
-        Student s_E = new Student(strArray[0].trim(), strArray[1].trim(), subArray_E[0].trim());
-        English en1 = new English(Integer.parseInt(subArray_E[1].trim()), Integer.parseInt(subArray_E[2].trim()),
-                Integer.parseInt(subArray_E[3].trim()));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(subArray_E[i]);
+        }
 
         ReadDataSub_S("test.txt", subArray_S, " ");
 
-        Student s_S = new Student(strArray[0].trim(), strArray[1].trim(), subArray_S[0].trim());
-        Science sc1 = new Science(Integer.parseInt(subArray_S[1].trim()), Integer.parseInt(subArray_S[2].trim()),
-                Integer.parseInt(subArray_S[3].trim()), Integer.parseInt(subArray_S[4].trim()));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(subArray_S[i]);
+        }
 
         ReadDataSub_M("test.txt", subArray_M, " ");
 
-        Student s_M = new Student(strArray[0].trim(), strArray[1].trim(),
-                subArray_M[0].trim());
-        Maths m1 = new Maths(Integer.parseInt(subArray_M[1].trim()),
-                Integer.parseInt(subArray_M[2].trim()),
-                Integer.parseInt(subArray_M[3].trim()), 
-                Integer.parseInt(subArray_M[4].trim()),
-                Integer.parseInt(subArray_M[5].trim()),
-                Integer.parseInt(subArray_M[6].trim()),
-                Integer.parseInt(subArray_M[7].trim()),
-                Integer.parseInt(subArray_M[8].trim()));
-
-        System.out.println(en1.totalMark());
-        System.out.println(sc1.totalMark());
-        System.out.println(m1.totalMark());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(subArray_M[i]);
+        }
 
     }
 
@@ -67,7 +59,7 @@ public class Main {
         } catch (IOException e1) {
             System.out.println("Error in File reading!!!");
         } catch (Exception e2) {
-
+            System.out.println("Error in processing!!!");
         }
 
     }
@@ -84,17 +76,17 @@ public class Main {
 
                 line = br.readLine();
                 details1 = line.split(split);
-                for (int y = 0; y < 10; y++) {
+                for(int y=0; y<10; y++){
                     details[y] = details1[y].trim();
                 }
-
+                
             }
 
             br.close();
         } catch (IOException e1) {
             System.out.println("Error in File reading!!!");
         } catch (Exception e2) {
-
+            System.out.println("Error in processing!!!");
         }
 
     }
@@ -122,7 +114,7 @@ public class Main {
         } catch (IOException e1) {
             System.out.println("Error in File reading!!!");
         } catch (Exception e2) {
-
+            System.out.println("Error in processing!!!");
         }
 
     }
@@ -151,56 +143,8 @@ public class Main {
         } catch (IOException e1) {
             System.out.println("Error in File reading!!!");
         } catch (Exception e2) {
-
+            System.out.println("Error in processing!!!");
         }
-
-    }
-
-    static double calAvg(int totalMark) {
-
-        double avg = totalMark / 3;
-
-        return Math.round(avg * 100) / 100.0;
-
-    }
-
-    static String findGrade(double totalMark) {
-        if (totalMark > 90) {
-            return "A";
-        } else if (totalMark > 80 && totalMark < 89) {
-            return "B";
-        } else if (totalMark > 70 && totalMark < 79) {
-            return "C";
-        } else if (totalMark > 60 && totalMark < 69) {
-            return "S";
-        } else {
-            return "F";
-        }
-    }
-
-    static void findGradeCount(String grade) {
-        int A_count = 0, B_count = 0, C_count = 0, S_count = 0, F_count = 0;
-
-        switch (grade) {
-            case "A":
-                A_count++;
-            case "B":
-                B_count++;
-            case "C":
-                C_count++;
-            case "S":
-                S_count++;
-            case "F":
-                F_count++;
-            default:
-                System.out.println("Invalid Input");
-        }
-
-        System.out.println("A's" + A_count);
-        System.out.println("B's" + B_count);
-        System.out.println("C's" + C_count);
-        System.out.println("S's" + S_count);
-        System.out.println("F's" + F_count);
 
     }
 
