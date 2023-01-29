834c798ed570340b085525a3eac56a8b0c4ee4d5
M-Samly <m.shamlymhmd@gmail.com>
2023-01-29 09:53:26 +0530

    Get data from user

--- Challenge_5/main.java
+++ Challenge_5/main.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class main {
-    
+    public static void main(String[] args) throws InterruptedException {
+        double perDayPayment;
+        int noOfDays;
+
+        Scanner s = new Scanner(System.in); // create scanner as s
+        System.out.print("Enter Per Day Payment: ");
+        perDayPayment = s.nextDouble(); 
+
+        System.out.print("Enter Number Of Days: ");
+        noOfDays = s.nextInt(); 
+    }
 }
