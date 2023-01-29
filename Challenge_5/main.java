66fb813ddfa6ebba9de37b18702027080019d647
M-Samly <m.shamlymhmd@gmail.com>
2023-01-29 10:34:23 +0530

    Call the calculate method

--- Challenge_5/main.java
+++ Challenge_5/main.java
@@ -1,36 +1,38 @@
 import java.util.Scanner;
 
 public class main {
-    private double perDayPayment;
-    private int noOfDays;
-    private double basicSalary;
+    private double perDayPay;
+    private int noOfDay;
+    private double basicSal;
     private double allowances;
     private double epf;
-    private double finalSalary;
+    private double finalSal;
     private double employerContribution;
 
     public static void main(String[] args) throws InterruptedException {
-        double perDayPayment;
-        int noOfDays;
+        double perDayPay;
+        int noOfDay;
 
         Scanner s = new Scanner(System.in); // create scanner as s
         System.out.print("Enter Per Day Payment: ");
-        perDayPayment = s.nextDouble();
+        perDayPay = s.nextDouble();
 
         System.out.print("Enter Number Of Days: ");
-        noOfDays = s.nextInt();
+        noOfDay = s.nextInt();
         System.out.println();
+        main cal = new main();
+        cal.calculate(perDayPay, noOfDay);
     }
 
-    public void calculate() throws InterruptedException {
+    public void calculate(double perDayPay, int noOfDay) throws InterruptedException {
 
-        basicSalary = perDayPayment * noOfDays; // main thread cal the basic salary
+        basicSal = perDayPay * noOfDay; // main thread cal the basic salary
 
-        Thread thread2 = new Thread(() -> allowances = basicSalary * 0.05); // thread 2 cal the allowances
+        Thread thread2 = new Thread(() -> allowances = basicSal * 0.05); // thread 2 cal the allowances
         thread2.join();
 
         Thread thread3 = new Thread(() -> { // thread 3 cal the epf
-            double totalMonthlyEarnings = basicSalary + allowances;
+            double totalMonthlyEarnings = basicSal + allowances;
             epf = totalMonthlyEarnings * 0.08;
 
             employerContribution = totalMonthlyEarnings * 0.12; // cal employer Contribution
@@ -38,9 +40,10 @@ public class main {
         thread3.start();
         thread3.join();
 
-        finalSalary = basicSalary + allowances + epf; // main thread cal final Salary
+        finalSal = basicSal + allowances + epf; // main thread cal final Salary
         System.out.println(employerContribution);
         System.out.println(epf);
-        System.out.println(finalSalary);
+        System.out.println(finalSal);
     }
+    
 }
